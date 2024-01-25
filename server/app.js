9b2f5ff09c1c4bfa84999b574ad96f49484d9e59
Midlaj maruthayil <88334471+midlajmarutha@users.noreply.github.com>
2024-01-25 18:33:51 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -139,8 +139,11 @@ wss.on('connection',(connection,req)=>{
     let cookies = req.headers.cookie;
     if (cookies){
         let cookieString=cookies.split(';').find(str => str.startsWith('token'))
+        console.log(cookieString)
+        console.log(process.env.JWT_SECRET_KEY)
         if(cookieString){
             let token = cookieString.split('=')[1]
+            console.log(token)
             if(token){
                 jwt.verify(token,process.env.JWT_SECRET_KEY,{},(err,userData)=>{
                     if(err) {console.log(err)}
