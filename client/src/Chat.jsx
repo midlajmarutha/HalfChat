02a62c5b36fd4e126b393184aa3b114c2a549715
midlajmarutha <midlajmarutha4@gmail.com>
2024-01-25 17:18:22 +0530

    test deployment

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -23,7 +23,7 @@ function Chat() {
   const [newContacts, setNewContacts] = useState([])
   const [onlinePeople, setOnlinePeople] = useState([])
   useEffect(() => {
-    const websocket = new WebSocket("ws://localhost:3000");
+    const websocket = new WebSocket("ws://halfchat.onrender.com/");
     setWs(websocket)
     websocket.addEventListener('message', handleMessage)
   }, [])
